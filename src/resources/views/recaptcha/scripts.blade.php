8c861598714295847ec0cb545d25b0125cf0492c
Nurdaulet <nurik9293709@gmail.com>
2020-03-11 21:29:36 +0600

    Update scripts.blade.php

--- src/resources/views/recaptcha/scripts.blade.php
+++ src/resources/views/recaptcha/scripts.blade.php
@@ -1,21 +1,19 @@
 <script src="{{ config('brandstudio.recaptcha.url') }}{{ config('brandstudio.recaptcha.public_key') }}"></script>
 <script>
     grecaptcha.ready(function() {
-        document.querySelectorAll('form').forEach((element) => {
-            element.addEventListener('submit', function(e) {
-                e.preventDefault();
-                let form = e.target,
-                    input = document.createElement('input');
+        document.addEventListener('submit', function(e) {
+            e.preventDefault();
+            let form = e.target,
+                input = document.createElement('input');
 
-                input.setAttribute('type', 'hidden');
-                input.setAttribute('name', 'g-recaptcha-response');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', 'g-recaptcha-response');
 
-                grecaptcha.execute("{{ config('brandstudio.recaptcha.public_key') }}", {action: {{ $recaptcha_action ?? 'undefined' }}}).then((token) => {
-                    input.setAttribute('value', token);
-                    form.appendChild(input);
-                    form.submit();
-                });
+            grecaptcha.execute("{{ config('brandstudio.recaptcha.public_key') }}", {action: {{ $recaptcha_action ?? 'undefined' }}}).then((token) => {
+                input.setAttribute('value', token);
+                form.appendChild(input);
+                form.submit();
             });
-        })
+        });
     });
 </script>
